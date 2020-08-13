bf9684f5889db6910176111d9a976e8214c71693
Isaac Rios <jirb1213@live.com.mx>
2020-08-13 17:29:00 -0500

    Working on the responsive view

--- script.js
+++ script.js
@@ -6,11 +6,19 @@ const $notificationBox=document.getElementById("notificationBox")
 const $userData=document.getElementById("userData");
 const $arrowTurn=document.getElementById("arrowTurn");
 const $showUserData=document.getElementById("showUserData");
+const $aboutMenu=document.getElementById("aboutMenu");
+const $aboutList=document.getElementById("aboutBoxList");
+
 
 $upload.addEventListener("click",()=>{
     $upload.classList.toggle('active');
     $dropDownUpload.classList.toggle('showList');    
 
+    $userData.classList.remove("active");
+    $arrowTurn.classList.remove("turn")
+    $showUserData.classList.remove("showList")
+    $aboutMenu.classList.remove('active');
+    $aboutList.classList.remove('showList');
     $bell.classList.remove("active");
     $notificationBox.classList.remove("showList")
 })
@@ -19,13 +27,48 @@ $bell.addEventListener('click',()=>{
     $upload.classList.remove("active");
     $dropDownUpload.classList.remove("showList");
 
+    $userData.classList.remove("active");
+    $arrowTurn.classList.remove("turn")
+    $showUserData.classList.remove("showList")
+    $aboutMenu.classList.remove('active');
+    $aboutList.classList.remove('showList');
+
     $bell.classList.toggle("active");
     $notificationBox.classList.toggle("showList")
 })
 
 $userData.addEventListener('click', ()=>{
+    $upload.classList.remove("active");
+    $dropDownUpload.classList.remove("showList");
+    $bell.classList.remove("active");
+    $notificationBox.classList.remove("showList");
+    $aboutMenu.classList.remove('active');
+    $aboutList.classList.remove('showList');
+
     $userData.classList.toggle("active");
     $arrowTurn.classList.toggle("turn")
     $showUserData.classList.toggle("showList")
 })
 
+$aboutMenu.addEventListener("click", ()=>{
+    $upload.classList.remove("active");
+    $dropDownUpload.classList.remove("showList");
+    $bell.classList.remove("active");
+    $notificationBox.classList.remove("showList");
+    $userData.classList.remove("active");
+    $arrowTurn.classList.remove("turn")
+    $showUserData.classList.remove("showList")
+
+    $aboutMenu.classList.toggle('active');
+    $aboutList.classList.toggle('showList');
+})
+// debe existir una condicion que detecte si algun elemento esta activo, para poder desactivarlo.
+// $body.addEventListener('click',(event)=>{
+//     $upload.classList.remove("active");
+//     $dropDownUpload.classList.remove("showList");
+//     $bell.classList.remove("active");
+//     $notificationBox.classList.remove("showList");
+//     $userData.classList.remove("active");
+//     $arrowTurn.classList.remove("turn");
+//     $showUserData.classList.remove("showList");
+// })
